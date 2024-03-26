a67d1936e6c4a2fe9aa62efbf449b58c19b7ef7b
Sam Dicken <sam.dicken@outlook.com>
2024-03-26 19:23:16 +0000

    Fixed framework. Encoding needed to be set.

--- Program.cs
+++ Program.cs
@@ -2,51 +2,52 @@
 
 class KanjiNumberQuiz
 {
+  // The basic 0-9 kanji numbers.
+  static readonly Dictionary<int, string> kanjiList = new()
+  {
+      { 0, "零" },
+      { 1, "一" },
+      { 2, "に" },
+      { 3, "三" },
+      { 4, "四" },
+      { 5, "五" },
+      { 6, "六" },
+      { 7, "七" },
+      { 8, "八" },
+      { 9, "九" },
+      {10, "十" },
+  };
   static void Main()
   {
+    
 
-    // int score = 0;
-    Dictionary<int, string> kanjiList = new Dictionary<int, string>
-    {
-        { 0, "零" },
-        { 1, "一" },
-        { 2, "に" },
-        { 3, "三" },
-        { 4, "四" },
-        { 5, "五" },
-        { 6, "六" },
-        { 7, "七" },
-        { 8, "八" },
-        { 9, "九" },
-        { 10, "十" },
-    };
+    // Sets console encoding so it can understand japanese characters.
+    Console.OutputEncoding = Encoding.UTF8;
+    Console.InputEncoding = Encoding.UTF8;
+    int score = 0;
 
-
-    Console.WriteLine(Convert.ToInt32('十'));
-
+    
 
     Random rnd = new(); // New Random Number object.
-    int number = rnd.Next(11);
+    int number = rnd.Next(100); // New random number between 0 and 11 (0-10 inc).
+    // number = 59;
 
     Console.WriteLine($"Write the kanji for {number}");
-    byte[] response = Encoding.Unicode.GetBytes(Console.ReadLine().ToString());
-    byte[] answer = Encoding.Unicode.GetBytes(kanjiList.GetValueOrDefault<int, string>(number).ToString());
-    //Decode
-    string r = Encoding.Unicode.GetString(response);
-    string a = Encoding.Unicode.GetString(answer);
-    Console.WriteLine($"R: {r}, A: {a}");
+
+    string? response = Console.ReadLine();
+    string? answer = GetAnswer(number);
     if (response == answer)
     {
-      Console.WriteLine("Correct!");
-      Console.WriteLine($"{number} is {response}!");
-      // score++;
+      Console.Write("Correct! ");
+      Console.WriteLine($"{number} is {answer}.");
+      score++;
     }
     else
     {
-      Console.WriteLine("Incorrect");
-      Console.WriteLine($"{number} is {answer}");
+      Console.Write("Incorrect! ");
+     Console.WriteLine($"{number} is actually {answer}.");
+      
     }
-    Console.ReadLine();
 
 
     // while (true)
@@ -57,13 +58,22 @@ class KanjiNumberQuiz
     // }
   }
 
-  static void PrintChars(string s)
+  /// <summary>
+  /// Only works for numbers up to 99
+  /// </summary>
+  /// <param name="num"></param>
+  /// <returns></returns＞
+  public static string? GetAnswer(int num)
   {
-    Console.WriteLine($"\"{s}\".Length = {s.Length}");
-    for (int i = 0; i < s.Length; i++)
-    {
-      Console.WriteLine($"s[{i}] = '{s[i]}' ('\\u{(int)s[i]:x4}')");
-    }
-    Console.WriteLine();
+    // If there is a specific entry in the kanji list, return early with that kanji.
+    if(kanjiList.ContainsKey(num)) return kanjiList.GetValueOrDefault(num);
+
+    string? s = string.Empty;
+    int remainder, quotient = Math.DivRem(num, 10, out remainder);
+    
+    s += kanjiList.GetValueOrDefault(quotient) + kanjiList.GetValueOrDefault(10);
+    s += kanjiList.GetValueOrDefault(remainder);
+
+    return s;
   }
 }
